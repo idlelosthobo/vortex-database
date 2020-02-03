adae87ff46516d55b9579a8af5a4ceb48a928c01
Nathan Johnson <drugen@gmail.com>
2020-02-03 14:04:34 -0700

    block update.

--- vortex/core/block.py
+++ vortex/core/block.py
@@ -1,7 +1,17 @@
+import os
+
 # this is the class that loads up a vdb file and operates it.
 
 
 class Block:
 
-    def __init__(self, _location):
+    def __init__(self, _location, _data_directory):
         self._location = _location
+        self._data_directory = _data_directory
+        self._data = None
+
+    def open_file(self):
+        data_file_location = os.path.join(self._data_directory, str(self._location) + '.vdb')
+        data_file = open(data_file_location, 'r')
+
+
