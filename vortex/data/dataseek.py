d2b566488bd354fc07cdcf5ba2de02cf4a5a6891
drugen <drugen@gmail.com>
2020-03-25 00:13:34 -0600

    Implementation of memory.py and getting data from blocks

--- vortex/data/dataseek.py
+++ vortex/data/dataseek.py
@@ -1,6 +1,10 @@
+import os
+from vortex.core import config
+
+
 #todo: Create a seek class and move everything into this file.
 #todo: Change the seek structure to use 0/1 instead of a count.
-
-
 class DataSeek:
-    pass
+
+    def __init__(self):
+        pass
